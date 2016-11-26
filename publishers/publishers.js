b566b47c1b7f16a7949300b96f8868016d4f7c76
Vishnu Bharathi <vishnubharathi04@gmail.com>
2016-11-26 12:05:33 +0530

    adds publisher option details

--- publishers/publishers.js
+++ publishers/publishers.js
@@ -1,13 +1,37 @@
 module.exports = (function () {
   return {
     github: {
-      publish: require('./github/publish.js')
+      publish: require('./github/publish.js'),
+      loginOpts: [
+        {
+          optName: 'username'
+        },
+        {
+          optName: 'token'
+        }
+      ]
     },
     gitlab: {
-      publish: require('./gitlab/publish.js')
+      publish: require('./gitlab/publish.js'),
+      loginOpts: [
+        {
+          optName: 'username'
+        },
+        {
+          optName: 'token'
+        }
+      ]
     },
     gitter: {
-      publish: require('./gitter/publish.js')
+      publish: require('./gitter/publish.js'),
+      loginOpts: [
+        {
+          optName: 'room'
+        },
+        {
+          optName: 'url'
+        }
+      ]
     }
   }
 })()
