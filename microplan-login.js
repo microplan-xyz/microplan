5d68091cdd496756d0d4f609e9d015f0437d7688
Vishnu Bharathi <vishnubharathi04@gmail.com>
2016-11-26 12:05:07 +0530

    adds login logic

--- microplan-login.js
+++ microplan-login.js
@@ -3,6 +3,11 @@ var fs = require('fs-extra')
 var path = require('path')
 var homeDir = require('home-dir')
 var credentialsLocation = '.microplan'
+var _ = require('underscore')
+var readline = require('readline')
+var util = require('util')
+var publishers = require('./publishers/publishers.js')
+var async = require('async')
 
 program
   .parse(process.argv)
@@ -14,16 +19,106 @@ if (loginArgs.length > 2) {
   process.exit(1)
 }
 
-try {
-  fs.writeFile(path.join(homeDir(), credentialsLocation), loginArgs[0], function (err) {
-    if (err) {
-      return console.log(err)
-    }
-    console.log('The credentials was saved at ' + homeDir())
-  })
-} catch (err) {
-  console.error(err)
-}
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-console.log('loginArgs: ', loginArgs[1])
-// args[1] has the
+var loginQuestion = 'Available publishers to log into : '
+var pubMap = []
+var pubIndex = 0
+_.each(
+  publishers,
+  function (pub, pubKey) {
+    var count = pubIndex + 1
+    pubMap[count] = {
+      type: pubKey,
+      config: pub
+    }
+    loginQuestion += util.format('\n\t%s. %s', count, pubKey)
+    pubIndex++
+  }
+)
+loginQuestion += '\nPlease choose publisher to login : '
+
+rl.question(loginQuestion,
+  function (answer) {
+    var publisher
+    try {
+      var optionAsNumber
+      optionAsNumber = parseInt(answer)
+      publisher = pubMap[optionAsNumber]
+      if (_.isEmpty(publisher)) {
+        throw new Error('Publisher Not Found')
+      }
+    } catch (ex) {
+      console.error('Invalid choice.')
+      process.exit(1)
+    }
+
+    var optQues = _.map(publisher.config.loginOpts,
+      function (opt) {
+        return function (auth, nextQuestion) {
+          rl.question(
+            util.format('%s %s ? ', publisher.type, opt.optName),
+            function (optAns) {
+              auth.loginOpts.push({
+                optName: opt.optName,
+                answer: optAns
+              })
+              nextQuestion(null, auth)
+            }
+          )
+        }
+      }
+    )
+
+    optQues.unshift(
+      function (callback) {
+        // initialize auth object for collecting option values from users
+        var auth = {
+          type: publisher.type,
+          loginOpts: []
+        }
+        callback(null, auth)
+      }
+    )
+
+    async.waterfall(
+      optQues,
+      function (err, auth) {
+        if (err) {
+          console.error(err)
+          process.exit(1)
+        }
+
+        var publisherCredentials
+        try {
+          var credFile = fs.readFileSync(path.join(homeDir(), credentialsLocation))
+          publisherCredentials = JSON.parse(credFile).publisherCredentials
+        } catch (er) {
+          // skip error, as .microplan file may not be there
+        }
+
+        publisherCredentials = _.isArray(publisherCredentials) ? publisherCredentials : []
+
+        fs.writeFile(
+          path.join(homeDir(), credentialsLocation),
+          JSON.stringify(
+            {
+              publisherCredentials: publisherCredentials.concat(auth)
+            }
+          ),
+          function (err) {
+            if (err) {
+              console.error(err)
+              process.exit(1)
+            }
+            console.log('The credentials was saved at ' + homeDir())
+            process.exit(0)
+          }
+        )
+      }
+    )
+  }
+)
